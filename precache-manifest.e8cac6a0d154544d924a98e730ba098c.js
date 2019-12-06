e5be9bbcdb3cc451aeae7ef8424e56ca19208ed2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-06 14:42:47 +0000

    Deploy feroxes/js-band-hw-task-7 to github.com/feroxes/js-band-hw-task-7.git:gh-pages

---
+++ precache-manifest.e8cac6a0d154544d924a98e730ba098c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "418a33792d21004d084b466c4ba6c842",
+    "url": "/js-band-hw-task-6/index.html"
+  },
+  {
+    "revision": "01e6b7e2f671ebfe06b7",
+    "url": "/js-band-hw-task-6/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "8028868c44c7df7a695e",
+    "url": "/js-band-hw-task-6/static/css/main.6dea8edd.chunk.css"
+  },
+  {
+    "revision": "01e6b7e2f671ebfe06b7",
+    "url": "/js-band-hw-task-6/static/js/2.57b4dc6b.chunk.js"
+  },
+  {
+    "revision": "8028868c44c7df7a695e",
+    "url": "/js-band-hw-task-6/static/js/main.6f9478f0.chunk.js"
+  },
+  {
+    "revision": "e9d523a2701100897758",
+    "url": "/js-band-hw-task-6/static/js/runtime-main.56c35154.js"
+  }
+]);
